2057f483be27750fdd811e000f9d31cdc2cbab5c
ruslan-barabanov <rem148esenin@gmail.com>
2021-01-19 19:29:51 +0300

    add base test

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,29 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void startTest() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://market.yandex.by/");
+    }
+
+    @AfterMethod
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
