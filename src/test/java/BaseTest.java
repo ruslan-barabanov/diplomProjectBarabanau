6e656ae628baac4c3e213ac71dfb1c53f67b4222
ruslan-barabanov <rem148esenin@gmail.com>
2021-02-01 20:26:01 +0300

    add property

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 
@@ -12,12 +14,19 @@ public class BaseTest {
 
     public WebDriver driver;
 
+    Properties properties = new Properties();
+
     @BeforeMethod
-    public void startTest() {
+    public void startTest() throws IOException {
+
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+//        properties.load(ClassLoader.getSystemResourceAsStream("conf.properties"));
+//        driver.get(properties.getProperty("yandex.market.page"));
+//        String yandexPage = properties.getProperty("yandex");
+//      driver.get(yandexPage);
         driver.get("https://market.yandex.by/");
     }
 
