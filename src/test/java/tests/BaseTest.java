9392819611124acda3753d2d8cad3e6ee92dbb0a
ruslan-barabanov <rem148esenin@gmail.com>
2021-02-23 01:28:31 +0300

    add assembly

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,12 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.PathsProperties;
+
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -13,12 +15,10 @@ public class BaseTest {
     public WebDriver driver;
     private final Properties properties = PathsProperties.readFile();
     private final String site = properties.getProperty("site");
-//    private final String chromeDriver = properties.getProperty("site");
 
     @BeforeMethod
     public void startTest() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Framework\\diplomProjectBarabanau\\src\\test\\resources\\chromedriver.exe");
-//      WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
